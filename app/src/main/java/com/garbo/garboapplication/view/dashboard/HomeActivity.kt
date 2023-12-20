ae2ddeb50b4989703ebe3acb57ef6520860915c6
atabiksf <82322722+atabiksf@users.noreply.github.com>
2023-12-21 02:00:33 +0700

    setup home activity layout

---
+++ app/src/main/java/com/garbo/garboapplication/view/dashboard/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.garbo.garboapplication.view.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.garbo.garboapplication.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
